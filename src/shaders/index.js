6f9cea27bbda5dbe26017d1666ce35202aa9071a
Ellen <shi.ellenduong@gmail.com>
2021-01-07 13:00:44 -0500

    added back in build folder

--- src/shaders/index.js
+++
@@ -1,6 +0,0 @@
-
-// This file exports available shaders to the GUI.
-// follow this syntax to make your shaders available to the GUI
-export {default as Lambert} from './lambert'
-export {default as ToonShader} from './toonshader'
-export {default as Iridescent} from './iridescent'
